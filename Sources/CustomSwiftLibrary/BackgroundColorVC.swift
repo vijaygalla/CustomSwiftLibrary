6f4196673c9e904da5563c9074df19afb4ec35bd
Vijay Bhaskar <kemisettykalyani308@gmail.com>
2023-11-13 17:44:42 +0530

    Changes done

--- Sources/CustomSwiftLibrary/BackgroundColorVC.swift
+++ Sources/CustomSwiftLibrary/BackgroundColorVC.swift
@@ -6,7 +6,20 @@ import UIKit
 open class BackgroundColorVC: UIViewController {
     override open func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .cyan
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 10).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10).isActive = true
+        // Do any additional setup after loading the view.
     }
+    
+    var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "Placeholder text view"
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.textColor = .gray
+        return label
+    }()
 }
